c16c4094ab6dc04a39aca0f089c71b3a70ab95a0
Qwire707 <132991214+Qwire707@users.noreply.github.com>
2023-06-10 19:36:47 +0300

    Add files via upload

--- pets/main.py
+++ pets/main.py
@@ -2,14 +2,14 @@ import play
 eat = True
 play.set_backdrop("purple")
 
-#коли програма прцює фнкція для початку гри
+#коли програма працює функція для початку гри
 @play.when_program_starts
 def start():
     global player, speech
     text1 = play.new_text(words="г - гладити, р - розсмішити, в - відпочивати" , x=0 , y=250, font_size=40)
     text2 = play.new_text(words="к - кормити, п - причепурити, пробіл - піти", x=0 , y=210, font_size=40)
     player = play.new_image(image="hello!.jpg", x=0, y=0, size=100)
-    speech = play.new_text(words="Hello my friend", x=0, y=-250, font_size=40)
+    speech = play.new_text(words="Привіт друже!", x=0, y=-250, font_size=40)
 
 
 #повторювати завжди(ігровий цикл)
@@ -19,38 +19,51 @@ async def do():
     if play.key_is_pressed("г"):
         await play.timer(1.0)
         player.image = "normX2 (1).png"
-        speech.words = "Good"
-        await play.timer(3.0)
+        speech.words = "Добре"
+        await play.timer(4.0)
         player.image = "SHIT_FLOPPER_008 (1) (1).png"
-        speech.words = "I'm in a bad mood today"
+        speech.words = "В мене сьогодні поганий настрій"
         await play.timer(3.0)
-        speech.words = "well, make me laugh quickly"
+        speech.words = "Тому спробуй мене розсмішити"
     if play.key_is_pressed("р"):
         await play.timer(2.0)
         player.image = "smile.jpg"
-        speech.words = "Nice joke*)"
+        speech.words = "Гарний жарт*)"
+        await play.timer(4.0)
+        player.image = "SHIT_FLOPPER_008 (1) (1).png"
+        speech.words = "Жартуєш ти добре, але час обідати"
+        await play.timer(3.0)
+        speech.words = "Швидко покорми мене!"
+
+
+
     if play.key_is_pressed("к"):
         await play.timer(1.0)
         player.image = "boss.jpg"
-        speech.words = "thanks for the food*)"
+        speech.words = "Дякую за обід*)"
+        await play.timer(3.0)
+        player.image = "boss2.jpg"
+        speech.words = "Тепер можна й відпочити"
     if play.key_is_pressed("в"):
         await play.timer(1.0)
         player.image = "lazy.jpg"
-        speech.words = "how to rest well*)"
+        speech.words = "Який гарний день для відпочинку.Чи не так?*)"
+        await play.timer(4.0)
+        player.image = "pon.jpg"
+        speech.words = "Сьогодні справді гарний день але..."
+        await play.timer(4.0)
+        speech.words = "подивись який я негарний, негайно причепури мене"
     if play.key_is_pressed("п"):
         await play.timer(1.0)
-        player.image = "pon.jpg"
-        speech.words = "don't touch me, I'm still resting"
-    if play.key_is_pressed("П"):
-        await play.timer(1.0)
         player.image = "Beautiful.jpg"
-        speech.words = "i am beautiful now"
+        speech.words = "Який я гарний"
     if play.key_is_pressed("space"):
         await play.timer(1.0)
         player.image = "bye.jpg"
-        speech.words = "are you leaving?"
+        speech.words = "Вже йдеш? Гарного дня"
         await play.timer(3.0)
-        speech.words = "come back soon, I'll be waiting*)"
+        speech.words = "Повертайся скоріше, я буду чекати на тебе*)"
+
 
 
 
